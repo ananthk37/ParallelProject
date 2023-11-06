fa568c86d18e57ca0b190aa865ed42947b414d00
Ananth Kumar <ananthkumar2002@tamu.edu>
2023-11-05 19:47:40 -0600

    finish selection sequential

---
+++ Builds/Selection_Seq/selection_seq.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+
+using namespace std;
+
+const char* data_init = "data_init";
+const char* comp = "comp";
+const char* comp_large = "comp_large";
+const char* comm = "comm";
+const char* correctness_check = "correctness_check";
+
+void random_fill(float* nums, int n) {
+    for(int i = 0; i < n; i++) {
+        nums[i] = rand() % n;
+    }
+}
+
+void sorted_fill(float* nums, int n) {
+    for(int i = 0; i < n; i++) {
+        nums[i] = i;
+    }
+}
+
+void reverse_fill(float* nums, int n) {
+    for(int i = 0; i < n; i++) {
+        nums[i] = n - i - 1;
+    }
+}
+
+void nearly_fill(float* nums, int n) {
+    for(int i = 0; i < n; i++) {
+        nums[i] = (rand() % n) / (n - i);
+    }
+}
+
+void selection_sort(float* nums, int n) {
+    for(int i = 0; i < n; i++) {
+        int min_index = i;
+        for(int j = i; j < n; j++) {
+            if(nums[min_index] > nums[j]) {
+                min_index = j;
+            }
+        }
+        //swap
+        float temp = nums[i];
+        nums[i] = nums[min_index];
+        nums[min_index] = temp;
+    }
+}
+
+bool confirm_sorted(float* nums, int n) {
+    for(int i = 0; i < n - 1; i++) {
+        if(nums[i] > nums[i + 1]) {
+            cout << "Correctness Check Failed..." << endl;
+            return false;
+        }
+    }
+    cout << "Correctness Check Passed!" << endl;
+    return true;
+}
+
+int main (int argc, char *argv[]) {
+    CALI_CXX_MARK_FUNCTION;
+    cali::ConfigManager mgr;
+    mgr.start();
+
+    // retrieve user input
+    const char* input_type = argv[1];
+    int size = atoi(argv[2]);
+    float* nums = new float [size];
+
+    // initialize data in array
+    CALI_MARK_BEGIN(data_init);
+    if(strcmp(input_type, "random") == 0) {
+        random_fill(nums, size);
+    }
+    if(strcmp(input_type, "sorted") == 0) {
+        sorted_fill(nums, size);
+    }
+    if(strcmp(input_type, "reverse") == 0) {
+        reverse_fill(nums, size);
+    }
+    if(strcmp(input_type, "nearly") == 0) {
+        nearly_fill(nums, size);
+    }
+    CALI_MARK_END(data_init);
+    cout << "Data Initialized" << endl;
+
+    // perform sort
+    CALI_MARK_BEGIN(comp);
+    CALI_MARK_BEGIN(comp_large);
+    selection_sort(nums, size);
+    CALI_MARK_END(comp_large);
+    CALI_MARK_END(comp);
+    cout << "selection Sort Completed" << endl;
+
+    // check for correctedness
+    CALI_MARK_BEGIN(correctness_check);
+    confirm_sorted(nums, size);
+    CALI_MARK_END(correctness_check);
+
+    // Metadata
+    adiak::init(NULL);
+    adiak::launchdate();    // launch date of the job
+    adiak::libraries();     // Libraries used
+    adiak::cmdline();       // Command line used to launch the job
+    adiak::clustername();   // Name of the cluster
+    adiak::value("Algorithm", "selection Sort"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+    adiak::value("ProgrammingModel", "Sequential"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
+    adiak::value("Datatype", "float"); // The datatype of input elements (e.g., double, int, float)
+    adiak::value("SizeOfDatatype", sizeof(float)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+    adiak::value("InputSize", size); // The number of elements in input dataset (1000)
+    adiak::value("InputType", input_type); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+    adiak::value("group_num", 3); // The number of your group (integer, e.g., 1, 10)
+    adiak::value("implementation_source", "Handwritten"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
+
+    mgr.stop();
+    mgr.flush();
+
+    delete[] nums;
+}
