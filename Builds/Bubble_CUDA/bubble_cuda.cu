df6e860233559944c8feddc768da456d81e0ba32
Robbie Clark <robbiec0720@gmail.com>
2023-11-14 16:43:02 -0600

    fixed adiak error

--- Builds/Bubble_CUDA/bubble_cuda.cu
+++ Builds/Bubble_CUDA/bubble_cuda.cu
@@ -261,7 +261,7 @@ int main(int argc, char *argv[]) {
     adiak::value("SizeOfDatatype", sizeof(float)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
     adiak::value("InputSize", NUM_VALS); // The number of elements in input dataset (1000)
     adiak::value("InputType", input_type); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
-    adiak::value("num_threads", NUM_VALS); // The number of CUDA or OpenMP threads
+    adiak::value("num_threads", NUM_THREADS); // The number of CUDA or OpenMP threads
     adiak::value("num_blocks", BLOCKS); // The number of CUDA blocks 
     adiak::value("group_num", 3); // The number of your group (integer, e.g., 1, 10)
     adiak::value("implementation_source", "AI"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
