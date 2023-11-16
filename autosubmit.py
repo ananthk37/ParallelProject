3f87bff5c5af2c8c994248216a321eb2558500d7
Robbie Clark <robbiec0720@gmail.com>
2023-11-15 18:35:24 -0600

    added analysis stuff

---
+++ autosubmit.py
@@ -0,0 +1,45 @@
+import os
+
+inputSize = [pow(2, 16), pow(2, 20), pow(2, 24)]
+processCounts = [2,4,8,16,32,64,128]
+cudaThreadCounts = [64,128,256,512,1024]
+inputTypes = ["sorted", "reverse", "nearly", "random"]
+size_per_thread = [pow(2, 6), pow(2, 10), pow(2, 14)]
+size_per_proc = [pow(2, 9), pow(2, 13), pow(2, 17)]
+
+def run_sequential():
+  for inputType in inputTypes:
+    for size in inputSize:
+      #note change file name to match your job
+      os.system("sbatch jobfile.sh {} {}".format(inputType, size))
+
+def run_MPI():
+   for inputType in inputTypes:
+    for size in inputSize:
+      for procs in processCounts:
+        #note change file name to match your job
+        os.system("sbatch mpi.grace_job {} {} {}".format(inputType, procs, size))
+
+def run_CUDA():
+  for inputType in inputTypes:
+    for size in inputSize:
+      for threads in cudaThreadCounts:
+        #note change file name to match your job
+        os.system("sbatch cuda.grace_job {} {} {}".format(inputType, threads, size))
+        
+def run_weak_MPI():
+    for procs in processCounts:
+        for size in size_per_proc:
+            os.system("sbatch mpi.grace_job random {} {}".format(procs, size*procs))
+            
+def run_weak_CUDA():
+    for threads in cudaThreadCounts:
+        for size in size_per_thread:
+            os.system("sbatch cuda.grace_job random {} {}".format(threads, size*threads))
+
+# run_sequential()
+# run_MPI()
+# run_CUDA()
+
+# run_weak_MPI()
+# run_weak_CUDA()
